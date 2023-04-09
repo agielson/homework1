d83d2de25b5e53e187ca55c053bdaaa178b91b86
agielson <122316882+agielson@users.noreply.github.com>
2023-04-09 21:30:14 +0300

    Create Program.cs

---
+++ лаба ооп 2.1/лаба ооп 2.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Project {
+    class MainClass {
+        public static void Main(string[] args) {
+            int a = 0; int b = 0;   
+            string str = "(5*(x+3))-1/(cos(x)))-2";
+            if (str.Contains(")")){
+                a++;
+            }
+            if (str.Contains("(")){ 
+                b++;
+            }
+            int c = a - b;
+            if (c == 0)
+            {
+                Console.WriteLine("All ok");
+            }
+            else
+            {
+                Console.WriteLine("You have mistake, my friend");
+            }
+        }
+    }
+}
